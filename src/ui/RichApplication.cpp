0549b7939cb73018a3fe3bf794a0811607cec039
ofix <shb8845369@gmail.com>
2024-05-15 17:36:29 +0800

    update meson.build project configuration

--- src/ui/RichApplication.cpp
+++ src/ui/RichApplication.cpp
@@ -2,20 +2,20 @@
 #include "ui/RichMainFrame.h"
 
 //(*AppHeaders
-#include "ui/RichMainFrame.h"
 #include <wx/image.h>
+#include "ui/RichMainFrame.h"
 //*)
 
 wxIMPLEMENT_APP(RichApplication);
 
 bool RichApplication::OnInit() {
     m_pDataCenter = new ShareDataCenter();
-    m_pDataCenter->Init(); // 进行必要的数据加载
+    m_pDataCenter->Init();  // 进行必要的数据加载
     //(*AppInitialize
     bool wxsOK = true;
     wxInitAllImageHandlers();
-    if ( wxsOK ) {
-        RichMainFrame* Frame = new RichMainFrame(NULL,wxID_ANY);
+    if (wxsOK) {
+        RichMainFrame* Frame = new RichMainFrame(NULL, wxID_ANY);
         Frame->Show();
         SetTopWindow(Frame);
     }
@@ -23,6 +23,6 @@ bool RichApplication::OnInit() {
     return true;
 }
 
-ShareDataCenter* RichApplication::GetDataCenter(){
-   return m_pDataCenter;
+ShareDataCenter* RichApplication::GetDataCenter() {
+    return m_pDataCenter;
 }
