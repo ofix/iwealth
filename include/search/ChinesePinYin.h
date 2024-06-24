bac34350744847850826f1cd7bbf3e33cc4b0ca3
ofix <shb8845369@gmail.com>
2024-06-24 23:40:35 +0800

    add chinese pin yin dictionary file

--- include/search/ChinesePinYin.h
+++ include/search/ChinesePinYin.h
@@ -2,10 +2,17 @@
 #define ChinesePinYin_H
 
 #include <string>
+#include <unordered_map>
+#include <vector>
 
 class ChinesePinYin {
    public:
     static std::string GetFirstLetters(const std::string& chinese);
+
+   private:
+    static bool LoadPinYinDictionary(const std::string& dict_path);
+    static bool m_inited = false;
+    static std::unordered_map<std::string, std::vector<std::string>> m_pinyin_dict;
 };
 
 #endif  // ChinesePinYin_H
